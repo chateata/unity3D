8ede00d01b6feaecc2815719c9d794a79ff724e5
Wang Weiwei <3523257792@qq.com>
2024-06-18 09:48:50 +0800

    MainMenuChange

--- Assets/Scripts/RunTime/Game/MainMenu/MainMenu.cs
+++ Assets/Scripts/RunTime/Game/MainMenu/MainMenu.cs
@@ -13,7 +13,14 @@ public class NewBehaviourScript : MonoBehaviour
 
    public void Quit()
    {
+        // Application.Quit();
+        // 在编辑器模式下，退出播放模式
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // 在构建后的应用中，退出游戏
         Application.Quit();
-   }
-   
+#endif
+    }
+
 }
