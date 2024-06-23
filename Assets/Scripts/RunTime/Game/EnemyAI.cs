372d3c21a36378bf0ca1fb72facd75fbb208e2d1
chateata <daiasj@foxmail.com>
2024-06-23 13:43:06 +0800

    敌人攻击13:43

--- Assets/Scripts/RunTime/Game/EnemyAI.cs
+++ Assets/Scripts/RunTime/Game/EnemyAI.cs
@@ -12,17 +12,20 @@ public class EnemyAI : MonoBehaviour
 
  
     private bool isAttacking = false;
+    private float chargeTimer = 0f;
+    private float chargeTime = 10f;
     private float attackTimer = 0f;
-    private float attackTime = 10f;
+    private float attackTime = 2f;
     private float playerLastXpos;
 
     public Transform playerTransform;
     public Transform EnemyTransform;
     private GameObject gameMgr;
     private GameObject chargeParticles = null;
-    public GameObject attackParticles;
-    public GameObject attackParticlesPrefab = null;
-
+    private GameObject attackParticles = null;
+    private GameObject chargeParticlesPrefab = null;
+    private GameObject attackParticlesPrefab = null;
+ 
 
    
     
@@ -35,7 +38,7 @@ public class EnemyAI : MonoBehaviour
 
         playerLastXpos = playerTransform.position.x;
 
-        chargeParticles = Resources.Load<GameObject>("Effects/Hovl Studio/Magic effects pack/Prefabs/Portals/Portal blue");
+        chargeParticlesPrefab = Resources.Load<GameObject>("Effects/Hovl Studio/Magic effects pack/Prefabs/Portals/Portal blue");
         attackParticlesPrefab = Resources.Load<GameObject>("Effects/Hovl Studio/Magic effects pack/Prefabs/Slash effects/Charge slash purple");
         
        
@@ -54,36 +57,63 @@ public class EnemyAI : MonoBehaviour
        
         if(zdistance_player <= attackRange + 0.1f )
         {
-            
-            isAttacking = true;
-            attackTimer += Time.deltaTime;
-
-            if (chargeParticles != null && !isAttacking)
+            if (!isAttacking)
             {
-                Debug.Log("Charge!");
-                Instantiate(chargeParticles, transform.position, Quaternion.identity);
+                if (chargeParticlesPrefab != null)
+                {
+                    chargeParticles = Instantiate(chargeParticlesPrefab, this.EnemyTransform.position, Quaternion.identity);
+                    chargeParticles.name = "chargeParticles" ;
+                    chargeParticles.transform.SetParent(this.EnemyTransform,false);
+                    chargeParticles.transform.localScale = new Vector3(6f, 6f, 6f);
+                    Debug.Log("Enemy is charging.");
+                }else{
+                    Debug.Log("No chargeParticles!");
+                }
             }
             
-            if( attackTimer >= attackTime )
+            isAttacking = true;
+            chargeTimer += Time.deltaTime;
+            
+            
+            if( chargeTimer >= chargeTime )
             {
-                AttackPlayer();
-                attackTimer = 0f;
-                isAttacking = false;
+                attackTimer += Time.deltaTime;
+                Destroy(chargeParticles);
+                if (attackParticles == null)
+                {
+                    AttackPlayer();
+                 
+                }
+
+                if (attackTimer >= attackTime)
+                {
+                    Destroy(attackParticles);
+                    attackTimer = 0f;
+                    chargeTimer = 0f;
+                    isAttacking = false;
+                }
             }
 
         }else
         {
-        
+            
             isAttacking = false;
-            attackTimer = 0f;
+            chargeTimer = 0f;
         }
 
-        if (attackParticles != null)
+       FollowEnemy(attackParticles);
+       FollowEnemy(chargeParticles);
+
+    }
+
+    void FollowEnemy (GameObject gameObject)
+    {
+         if (gameObject != null)
         {
-            attackParticles.transform.position = this.EnemyTransform.position;
-            attackParticles.transform.rotation = this.EnemyTransform.rotation;
-        }
+            Vector3 pos= new Vector3(this.EnemyTransform.position.x, this.EnemyTransform.position.y + 0.6f, this.EnemyTransform.position.z - 1);
+            gameObject.transform.position = pos;
 
+        }
     }
 
     void FollowPlayer()
@@ -91,17 +121,19 @@ public class EnemyAI : MonoBehaviour
         float targetX = playerTransform.position.x;
         transform.position = new UnityEngine.Vector3(Mathf.Lerp(transform.position.x, targetX, Time.deltaTime * 3f), 0f, transform.position.z);
         playerLastXpos = playerTransform.position.x;
-
+        
         
     }
 
     void AttackPlayer()
     {
-        if (attackParticles != null)
+        if (attackParticlesPrefab != null)
         {
             attackParticles = Instantiate(attackParticlesPrefab, this.EnemyTransform.position, Quaternion.identity);
             attackParticles.name = "attackParticles" ;
             attackParticles.transform.SetParent(this.EnemyTransform,false);
+            attackParticles.transform.Rotate(Vector3.forward * 90f);
+           
 
             Debug.Log("Enemy attacked the player!");
         }else{
