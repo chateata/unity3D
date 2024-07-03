32ddd21b8ce6bc57a81073086f5fd684a2366165
111Jessica111 <165466969+111Jessica111@users.noreply.github.com>
2024-07-03 09:50:50 +0800

    AUdio2

--- Assets/Scripts/RunTime/Game/AudioControl.cs
+++ Assets/Scripts/RunTime/Game/AudioControl.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class AudioControl : MonoBehaviour
 {
-    // [SerializeField] AudioSource GameAudio;
-    // [SerializeField] AudioSource StartAudio;
-    // [SerializeField] AudioSource SfxAudio;
+    [SerializeField] AudioSource GameAudio;
+    [SerializeField] AudioSource StartAudio;
+    [SerializeField] AudioSource SfxAudio;
 
-    // public AudioClip gameAudio;
-    // public AudioClip startAudio;
-    // public AudioClip dash;
-    // public AudioClip pickUp; 
-    // public AudioClip death;    
-    // public AudioClip attack;
-    // public AudioClip button;
-    // public AudioClip close_button;
+    public AudioClip gameAudio;
+    public AudioClip startAudio;
+    public AudioClip dash;
+    public AudioClip pickUp; 
+    public AudioClip death;    
+    public AudioClip attack;
+    public AudioClip button;
+    public AudioClip close_button;
 
-    // private void Start()
-    // {
-    //     GameAudio.clip=gameAudio;
-    //     GameAudio.Play();
-    // }
+    private void Start()
+    {
+        GameAudio.clip=gameAudio;
+        GameAudio.Play();
+    }
 }
