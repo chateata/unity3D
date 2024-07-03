293fcd388b6340af7e10c312b9d882dfda7ae851
chateata <daiasj@foxmail.com>
2024-07-03 08:33:34 +0800

    all'

--- Assets/Scripts/RunTime/Game/Crush.cs
+++ Assets/Scripts/RunTime/Game/Crush.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class Crush : MonoBehaviour
 {
-    
+    private GameObject HitParticles = null;
+    private GameObject HitParticlesPrefab = null;
+    public Transform VehicleTransform;
     void Start()
     {
-        
+        HitParticlesPrefab = Resources.Load<GameObject>("Effects/Matthew Guz/Hits Effects FREE/Prefab/Basic Hit 2");
     }
 
-   
+    private void GetHit(Vector3 pos)
+    {
+        HitParticles = Instantiate(HitParticlesPrefab, pos, Quaternion.identity);
+        HitParticles.name = "HitParticles" ;
+        HitParticles.transform.SetParent(this.VehicleTransform,false);
+        Time.timeScale=0;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.name=="Vehicle"){
-            Time.timeScale=0;
+        Debug.Log("OnTriggerEnter:碰撞到了"+ other.name);
+
+        if(other.tag=="Obstacle"){
+            
+            GameObject collidedObject = other.gameObject;
+            Vector3 collisionPosition = other.ClosestPoint(transform.position);
+            Debug.Log("Collided with " + collidedObject.name + " at position: " + collisionPosition);
+            GetHit(collisionPosition);
         }
         
     }
@@ -22,9 +37,18 @@ public class Crush : MonoBehaviour
     private void OnParticleCollision(GameObject other)
     {
         Debug.Log("碰撞到了"+ other.name);
+        if (other.name == "Slash")
+        {
+           
+        }
+
+
+
         if (other.name == "Vehicle")
         {
             Time.timeScale=0;
         }
+
+        
     }
 }
--- Assets/Scripts/RunTime/Game/TrailCtrl.cs
+++ Assets/Scripts/RunTime/Game/TrailCtrl.cs
@@ -16,7 +16,7 @@ public class TrailRendererController : MonoBehaviour
 
     private void SetTrailRendererProperties()
     {
-        trailRenderer.time = 0.4f;
+        trailRenderer.time = 0.2f;
         trailRenderer.widthCurve = AnimationCurve.Linear(0, 0.2f, 1, 0);
         trailRenderer.material = emissionMaterial;
         trailRenderer.minVertexDistance = 0.1f;
